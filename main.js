9332d138cde73197881c9ec7e0047f83e3af7a94
jorgepalis <96259400+jorgepalis@users.noreply.github.com>
2021-12-16 13:37:54 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', () =>{
+    document.querySelector('.nav-menu').classList.toggle("show");
+});
+
+ScrollReveal().reveal('.barner');
+ScrollReveal().reveal('.barner2', {delay: 200});
+ScrollReveal().reveal('.barner3', {delay: 200});
+ScrollReveal().reveal('.portales', {delay: 200});
